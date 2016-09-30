5e987065f9bd93376453086e637255483382da46
Liliya <liliyan2801@gmail.com>
2016-09-30 18:54:50 +0300

    task 3.0

--- task_3_0.cpp
+++ task_3_0.cpp
@@ -1,7 +1,46 @@
+#include <algorithm>
+#include <iomanip>
+#include <ios>
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-   cout << "hello";
+   cout << "Enter your name: ";
+   string name;
+   cin >> name;
+   cout << "\nHello, " << name << endl;
+
+   cout << "Enter your middle and final marks for exams: ";
+   double middterm, finalterm;
+   cin >> middterm >> finalterm;
+
+   cout << "Enter your marks for hometasks: ";
+   vector<double> homework;
+   double x;
+   while(cin >> x)
+      homework.push_back(x);
+
+   typedef vector<double>::size_type vec_size;
+   vec_size size = homework.size();
+   if(size == 0)  
+   {
+      cout << "empty";
+      return 1;
+   }
+
+   sort(homework.begin(), homework.end());
+   vec_size mid = size/2;
+   double median;
+   median = size % 2 == 0 ? (homework[mid] + homework[mid - 1]) / 2
+                          : homework[mid];
+
+   streamsize prec = cout.precision();
+   cout << "Your total mark is " << setprecision(3)
+   << 0.2 * middterm + 0.4 * finalterm + 0.4 * median
+   << setprecision(prec) << endl;
+
+   return 0;     
 }
