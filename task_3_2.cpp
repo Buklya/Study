8185987483187f26a4394849751242eed16e3933
lnikolenko <liliyan@ukr.net>
2016-10-04 16:28:09 +0300

    task 3.2

---
+++ task_3_2.cpp
@@ -0,0 +1,41 @@
+//Напишите программу, которая должна вычислить, сколько раз каждое отдельное слово содержится во введенных данных
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+   cout << "Enter some words: ";
+   vector<string> words;
+   string s;
+   while(cin >> s)
+      words.push_back(s);
+    
+   vector<string> uniqWords;
+   vector<int> count;
+   vector<string>::const_iterator it;   
+   for (int i = 0; i < words.size(); i++)
+   {      
+      it = find(uniqWords.begin(), uniqWords.end(), words[i]);
+      if(it != uniqWords.end())
+      {
+         int pos = it - uniqWords.begin();
+         count[pos] += 1;
+      }
+      else
+      {
+        uniqWords.push_back(words[i]);
+        count.push_back(1);
+      }
+   }   
+
+   for (int i = 0; i < uniqWords.size(); i++)
+   {
+      cout << uniqWords[i] << " " << count[i] << endl;      
+   }
+   
+   return 0;
+}
